a30bf49e0d1a8805cddfd93240020281a433bf8a
Michael Sackey <sackeymichael118@gmail.com>
2025-07-13 06:07:57 +0000

    Add files via upload

---
+++ rectangle_area.py
@@ -0,0 +1,11 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Define the length and width of the rectangle
+... length = 10
+... width = 5
+... 
+... # Calculate the area of the rectangle
+... area = length * width
+... 
+... # Print the calculated area in the specified format
+... print(f"The area of the rectangle is: {area}")
