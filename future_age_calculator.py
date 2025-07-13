0361441daee891df25f967caa77beaae2b88ea6e
Michael Sackey <sackeymichael118@gmail.com>
2025-07-13 06:14:01 +0000

    Add files via upload

---
+++ future_age_calculator.py
@@ -0,0 +1,11 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Define the number of hours to convert
+... hours = 2
+... 
+... # Calculate the number of seconds in the given hours
+... # There are 3600 seconds in an hour (60 minutes * 60 seconds/minute)
+... seconds = hours * 3600
+... 
+... # Print the result in the specified format
+... print(f"{hours} hour(s) is {seconds} seconds.")
